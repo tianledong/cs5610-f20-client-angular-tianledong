caca20f021e7dafbb2af7e9212a2468856b7f8ce
Tianle Dong <dtllawrence@gmail.com>
2020-11-11 21:59:08 -0500

    create course-navigator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseNavigatorComponent
   ],
   imports: [
     BrowserModule,
