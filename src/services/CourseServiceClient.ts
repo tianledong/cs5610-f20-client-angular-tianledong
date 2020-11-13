117ccdaf3737f9ea5edfe2384193768e36502bcf
Tianle Dong <dtllawrence@gmail.com>
2020-11-12 23:34:21 -0500

    create CourseServiceClient.ts

---
+++ src/services/CourseServiceClient.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+
+const courseUrl = 'https://wbdv-generic-server.herokuapp.com/api/tianledong/courses';
+
+@Injectable()
+export class CourseServiceClient {
+  findAllCourses = () =>
+    fetch(courseUrl)
+      .then(response => response.json())
+
+  deleteCourse = (courseId) =>
+    fetch(`${courseUrl}/${courseId}`, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+
+  createCourse = () =>
+    fetch(courseUrl, {
+      method: 'POST',
+      body: JSON.stringify({title: 'New Course', editing: false}),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+
+  updateCourse = (course) =>
+    fetch(`${courseUrl}/${course.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(course),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+}
