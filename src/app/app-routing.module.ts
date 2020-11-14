b922468dd3020f8a611ebbb76af9314d51fa95eb
Tianle Dong <dtllawrence@gmail.com>
2020-11-14 18:53:41 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CourseViewerComponentComponent} from './course-viewer-component/course-viewer-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CourseViewerComponentComponent},
+  {path: 'courses', component: CourseViewerComponentComponent},
+  {path: 'courses/:cid/modules', component: CourseViewerComponentComponent},
+  {path: 'courses/:cid/modules/:mid/lessons', component: CourseViewerComponentComponent},
+  {path: 'courses/:cid/modules/:mid/lessons/:lid/topics', component: CourseViewerComponentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
