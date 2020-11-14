4b7dc0be91cfe834e86e6b8d0b9b6a3cc425ebad
Tianle Dong <dtllawrence@gmail.com>
2020-11-14 18:39:30 -0500

    add providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CourseTableComponentComponent } from './course-table-component/course-table-component.component';
-import { CourseViewerComponentComponent } from './course-viewer-component/course-viewer-component.component';
-import { ModuleListComponentComponent } from './module-list-component/module-list-component.component';
-import { LessonTabsComponentComponent } from './lesson-tabs-component/lesson-tabs-component.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CourseTableComponentComponent} from './course-table-component/course-table-component.component';
+import {CourseViewerComponentComponent} from './course-viewer-component/course-viewer-component.component';
+import {ModuleListComponentComponent} from './module-list-component/module-list-component.component';
+import {LessonTabsComponentComponent} from './lesson-tabs-component/lesson-tabs-component.component';
+import {CourseServiceClient} from '../services/CourseServiceClient';
+import {LessonServiceClient} from '../services/LessonServiceClient';
+import {ModuleServiceClient} from '../services/ModuleServiceClient';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,8 @@ import { LessonTabsComponentComponent } from './lesson-tabs-component/lesson-tab
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CourseServiceClient, LessonServiceClient, ModuleServiceClient],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/services/LessonServiceClient.ts
+++ src/services/LessonServiceClient.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 const moduleUrl = 'https://wbdv-generic-server.herokuapp.com/api/tianledong/modules';
 
 @Injectable()
-export class ModuleServiceClient {
+export class LessonServiceClient {
   findLessonsForModule = (mid) =>
     fetch(`${moduleUrl}/${mid}/lessons`)
       .then(response => response.json())
