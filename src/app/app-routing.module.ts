8f806334a2ec269c6635e18468edf10172c90b28
Tianle Dong <dtllawrence@gmail.com>
2020-11-17 15:17:58 -0500

    separate course table and module to two pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CourseViewerComponent} from './course-viewer/course-viewer.component';
+import {CourseTableComponent} from './course-table/course-table.component';
 
 const routes: Routes = [
-  {path: '', component: CourseViewerComponent},
-  {path: 'table/courses', component: CourseViewerComponent},
+  {path: '', component: CourseTableComponent},
+  {path: 'table/courses', component: CourseTableComponent},
   {path: ':layout/courses/:cid/modules', component: CourseViewerComponent},
   {path: ':layout/courses/:cid/modules/:mid/lessons', component: CourseViewerComponent},
   {path: ':layout/courses/:cid/modules/:mid/lessons/:lid/topics', component: CourseViewerComponent},
