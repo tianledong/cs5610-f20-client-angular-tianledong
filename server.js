4a4d3e35c376557238b36099ddbdc9bcef41bb40
Tianle Dong <dtllawrence@gmail.com>
2020-11-23 17:26:04 -0500

    update for deploying on heroku

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/cs5610-f20-client-angular-tianledong'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-                         '/dist/cs5610-f20-client-angular-tianledong/index.html'));});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+                         '/dist/cs5610-f20-client-angular-tianledong/index.html'));
+});
 app.listen(process.env.PORT || 8080);
