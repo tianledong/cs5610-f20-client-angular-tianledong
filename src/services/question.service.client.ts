4fc9029a75f289fb23d3dc0220ee15d4a39e6b58
Tianle Dong <dtllawrence@gmail.com>
2020-11-23 18:11:46 -0500

    change url to https

--- src/services/question.service.client.ts
+++ src/services/question.service.client.ts
@@ -2,6 +2,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionsServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes/${qid}/questions`)
+    fetch(`https://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
--- src/services/quiz.service.client.ts
+++ src/services/quiz.service.client.ts
@@ -2,9 +2,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizzesServiceClient {
   findAllQuizzes = () =>
-    fetch('http://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes')
+    fetch('https://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
-    fetch(`http://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes/${qid}`)
+    fetch(`https://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 }
