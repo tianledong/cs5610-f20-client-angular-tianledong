78be7c2cac2dbf8f531d2f4606f92b0e1c4f1cda
Tianle Dong <dtllawrence@gmail.com>
2020-11-23 16:13:05 -0500

    change service url to remote

--- src/services/question.service.client.ts
+++ src/services/question.service.client.ts
@@ -2,6 +2,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionsServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    fetch(`http://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
--- src/services/quiz.service.client.ts
+++ src/services/quiz.service.client.ts
@@ -2,9 +2,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizzesServiceClient {
   findAllQuizzes = () =>
-    fetch('http://localhost:3000/api/quizzes')
+    fetch('http://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    fetch(`http://cs5610-f20-server-node-tianled.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 }
